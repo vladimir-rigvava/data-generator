4a5bf3ddfffbcb54d0c0b8e746a9240c23af7114
vladimir-rigvava <rigvava-vladimir@yandex.ru>
2020-01-14 23:14:22 +0300

    Adds more distribution types

--- datagen.py
+++ datagen.py
@@ -21,6 +21,86 @@ class DataGenerator():
         '''
         return r.beta(a, b, self.size)
 
+    def binomial(self, n, p):
+        '''
+        Parameters:\n
+        n: integer, >=0 . p: float, >=0.
+        '''
+        return r.binomial(n, p, self.size)
+
+    def exponential(self, scale):
+        '''
+        Parameters:\n
+        scale: float >=0.
+        '''
+        return r.exponential(scale, self.size)
+
+    def gamma(self, k, theta):
+        '''
+        Parameters:\n
+        k: float, >=0 . theta: float, >=0.
+        '''
+        return r.gamma(k, theta, self.size)
+
+    def geometric(self, p):
+        '''
+        Parameters:\n
+        p: float, >=0.
+        '''
+        return r.geometric(p, self.size)
+
+    def hypergeometric(self, ngood, nbad, nall)
+        '''Parameters:\n
+        ngood: integer, >=0.\n
+        nbad: integer, >=0.\n
+        nall: integer, >=1 and <=ngood+nbad.
+        '''
+        return r.hypergeometric(ngood, nbad, nall, self.size)
+
+    def laplace(self, mean, scale):
+        '''Parameters:\n
+        mean: float. scale: float, >=0.
+        '''
+        return r.laplace(mean, scale, self.size)
+
+    def logistic(self, mean, scale):
+        '''
+        Parameters:\n
+        mean: float. scale: float, >=0.
+        '''
+        return r.logistic(mean, scale, self.size)
+
+    def lognormal(self, mean, std):
+        '''
+        Parameters:\n
+        mean: float. std: float, >=0.
+        '''
+        return r.lognormal(mean, std, self.size)
+
+    def logarithmic(self, p):
+        '''
+        Parameters:\n
+        p: float, must be in range (0, 1).
+        '''
+        return r.logseries(p, self.size)
+
+    def multinomial(self, n, pr_of_vals):
+        '''
+        Parameters:\n
+        n: int, >=0.\n
+        pr_of_vals: list of float probabilities, sum must be = 1.
+        '''
+        return r.multinomial(n, pr_of_vals, self.size)
+
+    def negative_binomial(self, n, p):
+        '''
+        Parameters:\n
+        n: int, >0. p: float in range [0, 1].
+        '''
+        return r.negative_binomial(n, p, self.size)
+
+    def 
+
     def triangular(self, left, top, right):
         '''
         Parameters:\n
@@ -33,14 +113,19 @@ class DataGenerator():
     def normal(self, mean, std):
         '''
         Parameters:\n
-        mean: float. std: float.
+        mean: float. std: float >=0.
         '''
         return r.normal(mean, std, self.size)
+
+    def poisson(self, lam):
+        '''
+        Parameters:\n
+        lam: float, >0.
+        '''
+        return r.poisson(lam, self.size)
+
+    
 
     
 
     ###WIP###
-
-    
-
-print(DataGenerator.tri)
